dfdec95009b71d872d331a5d7aabba8a8f0d7932
Anzehlat <Anzehlat@gmail.com>
2023-12-08 08:35:00 -0700

    First commit for project 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Reflection.PortableExecutable;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        //make videos and create comments
+        Video video1 = new Video("Introduction to C#", "C# Expert", 275);
+        video1.AddComment("Robert", "Great tutorial!");
+        video1.AddComment("Mark", "I learned a lot. Thank you!");
+        video1.AddComment("Ally", "This is great. Could you make more videos like this?");
+
+        Video video2 = new Video("Web Development Basics", "CodeMaster", 530);
+        video2.AddComment("Stephanie", "Thanks for sharing!");
+        video2.AddComment("Kara", "Very informative.");
+        video2.AddComment("Lilah", "I have a question about CSS..");
+
+        Video video3 = new Video("Basic CSS and HTML", "Web Design for Starters", 625);
+        video3.AddComment("Zehla", "Great web design skills!");
+        video3.AddComment("Talia", "Can you add the links to the programs you used?");
+        video3.AddComment("Tele", "How do you change the font of the Header?");
+
+        //list of videos
+        List<Video> videoList = new List<Video> { video1, video2, video3 };
+
+        //info for videos
+        foreach (var video in videoList)
+        {
+            Console.WriteLine("Title:" + video.Title);
+            Console.WriteLine("Author:" + video.Author);
+            Console.WriteLine("Length:" + video.Length + " seconds");
+            Console.WriteLine("Number of Comments:" + video.GetNumComments());
+
+
+            //show the comments for video
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine("\t" + comment.CommenterName + " : " + comment.CommentText);
+            }
+
+            Console.WriteLine("\n"); //line break between videos
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,31 @@
+class Video
+{
+    public string Title { get; }
+    public string Author { get; }
+    public int Length { get; }
+    private List<Comment> Comments { get; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(string commenterName, string commentText)
+    {
+        Comment comment = new Comment(commenterName, commentText);
+        Comments.Add(comment);
+    }
+
+    public int GetNumComments()
+    {
+        return Comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return Comments;
+    }
+}
